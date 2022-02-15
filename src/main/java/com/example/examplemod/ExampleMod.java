31fd6a5de13e77275f3d53228958b8bb278e23a4
전재욱 <wjswodnr100@gmail.com>
2022-02-15 18:57:52 +0900

    Initial commit

---
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -0,0 +1,50 @@
+package com.example.examplemod;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.item.crafting.ShapedRecipes;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
+import org.apache.logging.log4j.Logger;
+
+import java.util.*;
+
+@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
+public class ExampleMod
+{
+    public static final String MODID = "lowfriction";
+    public static final String NAME = "Low Friction Mod";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+    private Block block;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        logger.info("test");
+
+        for (Block block : ForgeRegistries.BLOCKS) {
+            //block.setDefaultSlipperiness(0.1f);
+            //block.slipperiness = 1.0934095F;
+            block.slipperiness = 1.0934095F;
+        }
+    }
+
+
+}
